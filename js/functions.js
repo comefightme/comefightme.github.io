4e5c52c9c982adaef62e632176bad688acca00d1
comefightme <ee.ansheng@dhs.sg>
2017-03-06 21:18:57 +0800

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,13 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+// Get a reference to the database service
+var database = firebase.database();
+
+
+$(document).ready(function () {
+	$("#submit").click(function () {
+		database.ref().push($("input[type='text']").val());
+	});
+});
